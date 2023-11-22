c88373e43d5a7471a118aca179c437ca105159b2
EnzoFavret3il <favrete@3il.fr>
2023-11-22 16:48:26 +0100

    16h48

--- Main.java
+++ Main.java
@@ -2,17 +2,17 @@ public class Main {
     public static void main(String[] args) {
         // Problème 1
         int[] numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number at index " + i + ": " + numbers[i]);
         }
 
-        // Problème 2
+        // Problème 2 aka divison euclidienne 
         double result = calculateValue(5, 0);
         System.out.println("Result: " + result);
 
         // Problème 3
-        int count = 10;
-        while (count > 0) {
+        int count = 0;
+        while (count > 10) {
             System.out.println("Count: " + count);
             count += 2;
         }
@@ -20,22 +20,28 @@ public class Main {
         // Problème 4
         String message = "Bonjour";
         System.out.println("Longueur du message : " + message.length());
-        char character = message.charAt(10);
-        System.out.println("Caractère à l'indice 10 : " + character);
+        char character = message.charAt(4);
+        System.out.println("Caractère à l'indice 4 : " + character);
 
         // Problème 5
         String str1 = "Hello";
-        String str2 = new String("Hello");
+        String str2 = new String("Hellop");
         System.out.println("Comparaison de chaînes : " + (str1 == str2));
 
         // Problème 6
-        Main mainObj = null;
+        Main mainObj= new Main();
         mainObj.method();
     }
 
     // Méthode pour le problème de division par zéro
     public static int calculateValue(int a, int b) {
-        return a / b;
+    	if(a!=0&&b!=0) {
+    		return a / b;
+    	}
+    	else {
+    		return 0;
+    	}
+        
     }
 
     // Méthode pour le problème d'utilisation incorrecte d'une méthode de classe
